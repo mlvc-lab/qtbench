427356805c5a40faeb788b9a77ebdba6726a15a2
Donghoon Kim <dhkim2810@gmail.com>
2026-01-06 14:31:39 +0900

    Fix typo

--- deepcompressor/app/diffusion/config.py
+++ deepcompressor/app/diffusion/config.py
@@ -51,7 +51,7 @@ class DiffusionPtqRunConfig:
             Whether to skip generation.
         skip_eval (`bool`, *optional*, defaults to `False`):
             Whether to skip evaluation.
-        load_model (`str`, *optional*, defaults to `""`):
+        load_from (`str`, *optional*, defaults to `""`):
             Directory path to load the model checkpoint.
         save_model (`str`, *optional*, defaults to `""`):
             Directory path to save the model checkpoint.
@@ -63,7 +63,9 @@ class DiffusionPtqRunConfig:
     output: OutputConfig
     pipeline: DiffusionPipelineConfig
     eval: DiffusionEvalConfig
-    quant: DiffusionQuantConfig = field(metadata={omniconfig.ARGPARSE_KWARGS: {"prefix": ""}})
+    quant: DiffusionQuantConfig = field(
+        metadata={omniconfig.ARGPARSE_KWARGS: {"prefix": ""}}
+    )
     text: LlmQuantConfig | None = None
     text_cache: LlmCacheConfig = field(default_factory=LlmCacheConfig)
     seed: int = 12345
@@ -77,20 +79,28 @@ class DiffusionPtqRunConfig:
         # region set text encoder quanatization scale default dtype
         if self.text is not None and self.text.enabled_wgts:
             self.text.wgts.scale_dtypes = tuple(
-                ScaleUtils.infer_scale_dtypes(self.text.wgts.scale_dtypes, default_dtype=self.pipeline.dtype)
+                ScaleUtils.infer_scale_dtypes(
+                    self.text.wgts.scale_dtypes, default_dtype=self.pipeline.dtype
+                )
             )
         if self.text is not None and self.text.enabled_ipts:
             self.text.ipts.scale_dtypes = tuple(
-                ScaleUtils.infer_scale_dtypes(self.text.ipts.scale_dtypes, default_dtype=self.pipeline.dtype)
+                ScaleUtils.infer_scale_dtypes(
+                    self.text.ipts.scale_dtypes, default_dtype=self.pipeline.dtype
+                )
             )
         if self.text is not None and self.text.enabled_opts:
             self.text.opts.scale_dtypes = tuple(
-                ScaleUtils.infer_scale_dtypes(self.text.opts.scale_dtypes, default_dtype=self.pipeline.dtype)
+                ScaleUtils.infer_scale_dtypes(
+                    self.text.opts.scale_dtypes, default_dtype=self.pipeline.dtype
+                )
             )
         # endregion
         self.eval.num_gpus = min(torch.cuda.device_count(), self.eval.num_gpus)
         if self.eval.batch_size_per_gpu is None:
-            self.eval.batch_size_per_gpu = max(1, self.eval.batch_size // self.eval.num_gpus)
+            self.eval.batch_size_per_gpu = max(
+                1, self.eval.batch_size // self.eval.num_gpus
+            )
             self.eval.batch_size = self.eval.batch_size_per_gpu * self.eval.num_gpus
         else:
             self.eval.batch_size = self.eval.batch_size_per_gpu * self.eval.num_gpus
@@ -103,7 +113,9 @@ class DiffusionPtqRunConfig:
             data=self.quant.calib.data,
         )
         if self.quant.calib.path:
-            self.quant.calib.path = os.path.abspath(os.path.expanduser(self.quant.calib.path))
+            self.quant.calib.path = os.path.abspath(
+                os.path.expanduser(self.quant.calib.path)
+            )
         # endregion
         # region setup eval reference root
         self.eval.ref_root = self.eval.ref_root.format(
@@ -117,18 +129,30 @@ class DiffusionPtqRunConfig:
         # endregion
         # region setup cache directory
         if self.cache is not None:
-            if self.quant.enabled_wgts or self.quant.enabled_ipts or self.quant.enabled_opts:
+            if (
+                self.quant.enabled_wgts
+                or self.quant.enabled_ipts
+                or self.quant.enabled_opts
+            ):
                 self.cache.dirpath = self.quant.generate_cache_dirpath(
-                    root=self.cache.root, shift=self.pipeline.shift_activations, default_dtype=self.pipeline.dtype
+                    root=self.cache.root,
+                    shift=self.pipeline.shift_activations,
+                    default_dtype=self.pipeline.dtype,
                 )
-                self.cache.path = self.cache.dirpath.clone().add_children(f"{self.pipeline.name}.pt")
+                self.cache.path = self.cache.dirpath.clone().add_children(
+                    f"{self.pipeline.name}.pt"
+                )
             else:
                 self.cache.dirpath = self.cache.path = None
         if self.text is not None and self.text.is_enabled():
             if not self.text_cache.root:
                 self.text_cache.root = os.path.join(self.cache.root, "diffusion")
-            self.text_cache.dirpath = self.text.generate_cache_dirpath(root=self.text_cache.root, seed=self.seed)
-            self.text_cache.path = self.text_cache.dirpath.clone().add_children(f"{self.pipeline.name}.pt")
+            self.text_cache.dirpath = self.text.generate_cache_dirpath(
+                root=self.text_cache.root, seed=self.seed
+            )
+            self.text_cache.path = self.text_cache.dirpath.clone().add_children(
+                f"{self.pipeline.name}.pt"
+            )
         # endregion
         # region setup output directory
         if self.output.dirname == "reference":
@@ -150,7 +174,9 @@ class DiffusionPtqRunConfig:
                 calib_dirname,
                 self.output.dirname,
             )
-        if (self.eval.chunk_start > 0 or self.eval.chunk_step > 1) and not self.eval.chunk_only:
+        if (
+            self.eval.chunk_start > 0 or self.eval.chunk_step > 1
+        ) and not self.eval.chunk_only:
             self.output.job += f".c{self.eval.chunk_start}.{self.eval.chunk_step}"
         # endregion
         diffusers.training_utils.set_seed(self.seed)
